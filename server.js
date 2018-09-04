6aec34133ab054a5c83f6347bad1970040f044df
Jasmine Fitts <jasminefitts@Jasmines-MacBook-Pro.local>
2018-09-03 23:18:32 -0400

    added server.js

--- server.js
+++ server.js
@@ -5,3 +5,19 @@ var mongoose = require("mongoose");
 
 var axios = require("axios");
 var cheerio = require("cheerio");
+
+//Requires all models
+var db = require("./models");
+
+var PORT = 3000;
+
+//Initialize Express
+var app = express();
+
+// Use morgan logger for logging requests
+// app.use(logger("dev"));
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: true }));
+// Use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
+
