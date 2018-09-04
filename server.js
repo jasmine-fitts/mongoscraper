20e12495676b6660e16a76f06377601a219f47ea
Jasmine Fitts <jasminefitts@Jasmines-MacBook-Pro.local>
2018-09-03 23:11:23 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,7 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+var axios = require("axios");
+var cheerio = require("cheerio");
